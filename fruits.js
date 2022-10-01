8601828cdb96afb804b4984130b529f82e5118ae
BryanJaraGit <brjara77@gmail.com>
2022-10-01 15:30:41 -0500

    Agregando proyecto

---
+++ fruits.js
@@ -0,0 +1,45 @@
+// Push Fruits
+// const fruits = [
+//     'Blackberry',
+//     'Apple'
+// ];
+
+// const addFruits = fruits.push('Mango');
+
+// console.log(fruits);
+// console.log(addFruits);
+
+
+// Sort fruits
+// const fruits = [
+//     'Blackberry',
+//     'Apple'
+// ];
+// fruits.sort();
+// console.log(fruits);
+
+// Pop fruits
+// const fruits = [
+//     'Blackberry',
+//     'Apple',
+//     'Uvas',
+//     'Melon'
+// ];
+
+// const eliminarUltimo = fruits.pop();
+// console.log(fruits);
+
+// Splice fruits
+const fruits = [
+    'Blackberry',
+    'Apple',
+    'Uvas',
+    'Melon',
+    'Melon'
+];
+
+const newArray = fruits.slice(1, 4, 'Lemon');
+
+console.log(fruits);
+
+console.log(newArray);
---
+++ verbs.js
@@ -0,0 +1,26 @@
+const verbs = [
+    'open',
+    'close',
+    'up',
+    'down'
+];
+
+const verbos = [
+    'abrir',
+    'cerrar',
+    'arriba',
+    'abajo'
+];
+
+const verb1 = document.getElementById("first-verb");
+const verb2 = document.getElementById("second-verb");
+const verb3 = document.getElementById("third-verb");
+const verb4 = document.getElementById("four-verb");
+
+verb1.innerHTML= verbs[0];
+verb2.innerHTML= verbs[1];
+verb3.innerHTML= verbs[2];
+verb4.innerHTML= verbs[3];
+
+
+
